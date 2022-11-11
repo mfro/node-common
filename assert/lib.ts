5d220c589a7615e23ca8a2d5b3407b9d24b6c0e4
Max Froehlich <max@mfro.me>
2022-11-11 12:02:55 -0800

    Add todo to assert

--- assert/lib.ts
+++ assert/lib.ts
@@ -1,3 +1,8 @@
+export function todo(message?: string): never {
+  debugger;
+  throw new Error(message);
+}
+
 export function assert(condition: boolean, message: string): asserts condition {
   if (!condition) {
     debugger;
